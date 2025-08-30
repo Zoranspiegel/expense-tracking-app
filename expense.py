a87e0c073cb7cd4f06da6886fda23b51e62b48f3
Juan José Rodriguez <zoranbow@gmail.com>
2025-08-30 14:03:32 -0500

    Expense class

---
+++ expense.py
@@ -0,0 +1,7 @@
+class Expense:
+    def __init__(self, name: str, category: str, amount: float) -> None:
+        self.name = name
+        self.category = category
+        self.amount = amount
+
+
